f158435ed2540b9422b51857456b91c704959ab9
chitrakshtarun <65770420+chitrakshtarun@users.noreply.github.com>
2025-12-24 23:19:53 +0530

    chore: setup nativewind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,11 @@
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { Slot } from 'expo-router'
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+import "@/global.css";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { Slot } from "expo-router";
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 if (!publishableKey) {
-  throw new Error(
-    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
-  )
+  throw new Error("Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env");
 }
 
 const _layout = () => {
@@ -14,7 +13,7 @@ const _layout = () => {
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <Slot />
     </ClerkProvider>
-  )
-}
+  );
+};
 
-export default _layout
+export default _layout;
